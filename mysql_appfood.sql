b79e97e496e7b5531f0f4a70edf1f213bbdc5fda
Nhut Quang <130155056+Nikko-Kun@users.noreply.github.com>
2023-09-23 00:07:25 +0900

    Add files via upload

---
+++ mysql_appfood.sql
@@ -0,0 +1,13 @@
+INSERT INTO `user` (`user_id`, `full_name`) VALUES
+(6, 'David Williams'),
+(7, 'Sarah Davis'),
+(10, 'Christopher Anderson'),
+(11, 'Lisa Thomas'),
+(12, 'Matthew Jackson'),
+(13, 'Elizabeth White'),
+(15, 'Jessica Wilson'),
+(16, 'William Martin'),
+(17, 'Megan Garcia'),
+(18, 'Richard Rodriguez'),
+(19, 'Amanda Martinez'),
+(20, 'Thomas Hernandez');
